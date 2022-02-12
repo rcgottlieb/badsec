4256106686627e95ec370392fdff6b8b7f1ad55c
Robert Gottlieb <rcgottlieb@icloud.com>
2022-02-11 21:15:49 -0500

    Figured out generators, put one in place for converting the json to a python list to better scale Also join url components using urllib.urljoin() rather than +

--- badsec.py
+++ badsec.py
@@ -4,6 +4,7 @@ import urllib.request
 import urllib.response
 from hashlib import sha256
 from urllib.error import HTTPError
+from urllib.parse import urljoin
 
 connection_attempts = 0
 base_url = 'http://0.0.0.0:8888'
@@ -12,33 +13,29 @@ base_url = 'http://0.0.0.0:8888'
 def connect_and_print_user_ids():
     auth_token = get_auth_token()
     user_ids = get_users_response(auth_token)
-    print_users_json(user_ids)
+    print(user_ids)
 
 
 def get_users_response(auth_token) -> str:
     """
     This function return's a json representation of the requested user ids as a python list of strings
-    :param auth_token: is the token retrieved from the server using get_auth_token().
+    :param auth_token: is the token retrieved from the server
     """
-    json_lines = []
     user_headers = dict()
     user_headers['X-Request-Checksum'] = auth_token
-    users_url = base_url + '/users'
+    users_url = urljoin(base_url, "/users")
     users_request = urllib.request.Request(users_url, headers=user_headers, method='GET')
     users_response = connect_with_request(users_request)
 
     # convert the lines to a list of python strings
     lines = users_response.readlines()
-    for line in lines:
-        json_line = line.decode('utf-8').strip()
-        json_lines.append(json_line)
-    user_ids: str = json.dumps(json_lines)
+    user_ids: str = json.dumps(list(line.decode('utf-8').strip() for line in lines))
     return user_ids
 
 
 def get_auth_token():
     global base_url
-    auth_url = base_url + "/auth"
+    auth_url = urljoin(base_url, "/auth")
     auth_request = urllib.request.Request(auth_url, method='HEAD')
     auth_response = connect_with_request(auth_request)
     auth_token_header = auth_response.headers['Badsec-Authentication-Token']
@@ -81,10 +78,6 @@ def connect_to_badsec_server_using_request(a_request):
         print("URL Error: ", error_response.reason, file=sys.stderr)
 
     return server_response, succeeded
-
-
-def print_users_json(user_ids):
-    print(user_ids)
 
 
 if __name__ == "__main__":
